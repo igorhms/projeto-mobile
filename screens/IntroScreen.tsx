809947419c33e59ddd9345ae94e449fb9fee7c39
igorhms <igorhms98@gmail.com>
2022-01-12 21:22:49 -0300

    SOSAPP002 - Adicionando imagens nas telas

--- screens/IntroScreen.tsx
+++ screens/IntroScreen.tsx
@@ -9,17 +9,17 @@ const data = [
   {
     title: "Save time by tracking your studies",
     text: "Schedule your classes, assignments, quizzes and more",
-    // image: require('../assets/images/splash.png'),
+    image: require('../assets/images/intro1.png'),
   },
   {
     title: "Stay on top of your education",
     text: "Reduce your stress, increase your productivity",
-    // image: require('../assets/images/splash.png'),
+    image: require('../assets/images/intro2.png'),
   },
   {
     title: "Spend more time doing the things you love",
     text: "Get started within five minutes",
-    // image: require('../assets/images/splash.png'),
+    image: require('../assets/images/intro3.png'),
   },
 ];
 
@@ -95,6 +95,8 @@ const styles = StyleSheet.create({
   },
   image: {
     marginVertical: 60,
+    width: 250,
+    height: 250,
   },
   title: {
     fontSize: 24,
